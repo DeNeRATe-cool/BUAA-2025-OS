01c8db40e23c4246e4d8ebfcbd2706f17aef7403
杨佳宇轩 <22375080@buaa.edu.cn>
2025-02-26 11:05:55 +0800

    first palindrome

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+
+int check(int n) {
+	int bit[5];
+	int len = 0;
+	while(n) bit[len++] = n % 10, n /= 10;
+	if(len == 0) return 1;
+	for(int i = 0; i < len; i++) {
+		if(bit[i] != bit[len - i - 1])
+			return 0;
+	}
+	return 1;
+
+}
+
 int main() {
 	int n;
 	scanf("%d", &n);
 
-	if () {
+	if (check(n)) {
 		printf("Y\n");
 	} else {
 		printf("N\n");
