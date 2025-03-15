757951899456308f82d6a5597ac9a316884b4de8
杨佳宇轩 <22375080@buaa.edu.cn>
2025-03-15 16:41:02 +0800

    3 lab1

--- lib/print.c
+++ lib/print.c
@@ -10,33 +10,58 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 	const char *s;
 	long num;
 
-	int width;
+	int width; // 标记输出宽度
 	int long_flag; // output is long (rather than int)
 	int neg_flag;  // output is negative
 	int ladjust;   // output is left-aligned
-	char padc;     // padding char
+	char padc;     // padding char 填充多余位置所用的字符
 
 	for (;;) {
 		/* scan for the next '%' */
+		width = 0;
+		long_flag = 0;
+		neg_flag = 0;
+		ladjust = 0;
+		padc = ' ';
 		/* Exercise 1.4: Your code here. (1/8) */
-
+		const char *p = fmt;
+		while((*p) != '%' && (*p) != '\0') {
+			p++;
+		}
 		/* flush the string found so far */
 		/* Exercise 1.4: Your code here. (2/8) */
-
+		out(data, fmt, p - fmt);
+		fmt = p;
 		/* check "are we hitting the end?" */
 		/* Exercise 1.4: Your code here. (3/8) */
-
+		if((*p) == '\0') 
+			break;
 		/* we found a '%' */
 		/* Exercise 1.4: Your code here. (4/8) */
-
+		fmt++;
 		/* check format flag */
 		/* Exercise 1.4: Your code here. (5/8) */
-
+		if((*fmt) == '-') {
+			ladjust = 1;
+			fmt++;
+		} else if((*fmt) == '0') {
+			padc = '0';
+			fmt++;
+		}
 		/* get width */
 		/* Exercise 1.4: Your code here. (6/8) */
-
+		if((*fmt) >= '0' && (*fmt) <= 9) {
+			while((*fmt) >= 0 && (*fmt) <= 9) {
+				width = width * 10 + ((*fmt) - '0');
+				fmt++;
+			}
+		}
 		/* check for long */
 		/* Exercise 1.4: Your code here. (7/8) */
+		if((*fmt) == 'l') {
+			long_flag = 1;
+			fmt++;
+		}
 
 		neg_flag = 0;
 		switch (*fmt) {
@@ -63,7 +88,11 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			 * others. (hint: 'neg_flag').
 			 */
 			/* Exercise 1.4: Your code here. (8/8) */
-
+			if(num < 0) {
+				neg_flag = 1;
+				num = -num;
+			}
+			print_num(out, data, num, 10, neg_flag, width, ladjust, padc, 0);
 			break;
 
 		case 'o':
