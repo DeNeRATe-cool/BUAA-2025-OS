d11b162491bf1e3fdec83f349e3c4e4c1ecdaac0
杨佳宇轩 <22375080@buaa.edu.cn>
2025-05-09 21:01:37 +0800

    lab5 test 1 v1

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -453,8 +453,21 @@ int sys_cgetc(void) {
  */
 int sys_write_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (1/2) */
+	if (is_illegal_va_range(va, len)) {
+		return -E_INVAL;
+	}
 
-	return 0;
+	if (len != 1 && len != 2 && len != 4) {
+		return -E_INVAL;
+	}
+
+	if ((0x180003f8 <= pa && pa + len <= 0x18000418) ||
+	    (0x180001f0 <= pa && pa + len <= 0x180001f8)) {
+		memcpy((void *)(KSEG1 | pa), (void *)va, len);
+		return 0;
+	}
+
+	return -E_INVAL;
 }
 
 /* Overview:
@@ -474,8 +487,22 @@ int sys_write_dev(u_int va, u_int pa, u_int len) {
  */
 int sys_read_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (2/2) */
+	if (is_illegal_va_range(va, len)) {
+		return -E_INVAL;
+	}
 
-	return 0;
+	if (len != 1 && len != 2 && len != 4) {
+		return -E_INVAL;
+	}
+
+	if ((0x180003f8 <= pa && pa + len <= 0x18000418) ||
+	    (0x180001f0 <= pa && pa + len <= 0x180001f8)) {
+		// 调用 memcpy 从设备读入内存
+		memcpy((void *)va, (void *)(KSEG1 | pa), len);
+		return 0;
+	}
+
+	return -E_INVAL;
 }
 
 void *syscall_table[MAX_SYSNO] = {
